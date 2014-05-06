43c67624ea85142eed4c1ce3ba1a31cf593577b4
Arrian Purcell <arrian.purcell@gmail.com>
2014-05-06 22:26:52 +1000

    Adding smb project.

---
+++ AndroidSMB/src/main/java/com/sevensimplex/androidsmb/MainActivity.java
@@ -0,0 +1,76 @@
+package com.sevensimplex.androidsmb;
+
+import android.app.Activity;
+import android.app.ActionBar;
+import android.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.os.Build;
+
+import org.alfresco.jlan.server.config.ServerConfiguration;
+import org.alfresco.jlan.smb.*;
+import org.alfresco.jlan.smb.server.*;
+import org.alfresco.jlan.netbios.*;
+
+import java.io.IOException;
+
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        try {
+            ServerConfiguration config = new ServerConfiguration("Android Server");
+
+            SMBServer server = new SMBServer(config);
+            server.startServer();
+
+        } catch (IOException e) {
+
+        }
+
+
+
+        if (savedInstanceState == null) {
+            getFragmentManager().beginTransaction()
+                    .add(R.id.container, new PlaceholderFragment())
+                    .commit();
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.action_settings:
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    public static class PlaceholderFragment extends Fragment {
+
+        public PlaceholderFragment() {
+        }
+
+        @Override
+        public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                Bundle savedInstanceState) {
+            View rootView = inflater.inflate(R.layout.fragment_main, container, false);
+            return rootView;
+        }
+    }
+
+}
